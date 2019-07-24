a6bb922f201b84f6281d0cc38ba140f42dbc65a8
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-07-24 14:45:24 -0600

    Form added and rendered

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,19 +1,41 @@
 import React, { useState } from 'react';
 
-import MemberCard from './components/MemberCard';
+import MemberCards from './components/MemberCards';
+import Form from './components/Form';
 
 import './App.css';
 
 function App() {
 
   const [members, setMembers] = useState([{ name: "Levi Thomas", email: "levithomasfswd@gmail.com", role: "Front-End Developer" }])
+  const [newmember, setNewMember] = useState({ name: "", email: "", role: "" })
+  const [memberToEdit, setMemberToEdit] = useState([])
+
+  function handleChange(event) {
+    const updatedMember = { ...newmember, [event.target.name]: event.target.value };
+    console.log(
+      "handleChange",
+      event.target.name,
+      event.target.value,
+      updatedMember
+    );
+    setNewMember(updatedMember);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setMembers([...members ].push(newmember))
+    console.log("Members", members);
+    setNewMember({ name: "", email: "", role: "" })
+  }
 
   return (
     <div className="App">
       <h1 className="title">Team Members:</h1>
       <div className="cards">
-        {members.map((member) => {return <MemberCard name={member.name} email={member.email} role={member.role}/>})}
+        <MemberCards memberlist={members}/>
       </div>
+      <Form submit={handleSubmit} membername={newmember.name} memberemail={newmember.email} memberrole={newmember.role} change={handleChange}/>
     </div>
   );
 }
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,3 +1,45 @@
 import React from 'react';
 
-function Form (props)
+function Form (props) {
+
+    return (
+    <div className="new-member-form">
+      <form onSubmit={props.submit}>
+        <label>
+          Member Name:
+          <input 
+          className="form-control"
+          type="text" 
+          name="name"
+          placeholder="Enter member name"
+          value={props.membername}
+          onChange={props.change}/>
+        </label>
+        <label>
+          Email:
+          <input 
+          type="email"
+          className="form-control"
+          name="email"
+          aria-describedby="emailHelp"
+          placeholder="Enter member email"
+          value={props.memberemail}
+          onChange={props.change}/>
+        </label>
+        <label>
+          Role:
+          <input 
+          type="text"
+          className="form-control"
+          name="role"
+          placeholder="Enter member role"
+          onChange={props.change}
+          value={props.memberrole}/>
+        </label>
+        <button type="submit">Submit New Member</button>
+      </form>
+    </div>
+    );
+}
+
+export default Form;
