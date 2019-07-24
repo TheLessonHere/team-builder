59f5af8a9b6b20a152956f560e55819a923dc58f
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-07-24 17:08:43 -0600

    Input form rendering, changed states on Form.js and App.js, still not functional

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,41 +1,29 @@
-import React, { useState } from 'react';
-
-import MemberCards from './components/MemberCards';
+import React, { useState, useEffect } from 'react';
 import Form from './components/Form';
 
 import './App.css';
 
 function App() {
 
-  const [members, setMembers] = useState([{ name: "Levi Thomas", email: "levithomasfswd@gmail.com", role: "Front-End Developer" }])
-  const [newmember, setNewMember] = useState({ name: "", email: "", role: "" })
+  const [members, setMembers] = useState([ { name:"", email:"", role:"" } ])
   const [memberToEdit, setMemberToEdit] = useState([])
 
-  function handleChange(event) {
-    const updatedMember = { ...newmember, [event.target.name]: event.target.value };
-    console.log(
-      "handleChange",
-      event.target.name,
-      event.target.value,
-      updatedMember
-    );
-    setNewMember(updatedMember);
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setMembers([...members ].push(newmember))
-    console.log("Members", members);
-    setNewMember({ name: "", email: "", role: "" })
-  }
+  useEffect(() => {
+    console.log(members)
+  }, [members])
 
   return (
     <div className="App">
       <h1 className="title">Team Members:</h1>
       <div className="cards">
-        <MemberCards memberlist={members}/>
+      <div className="member-card">
+            <h2 className="member-name">{members.name}</h2>
+            <p className="member-email">{members.email}</p>
+            <p className="member-role">{members.role}</p>
+            <button onClick={() => (console.log('button clicked'))}>Edit Member</button>
+        </div>
       </div>
-      <Form submit={handleSubmit} membername={newmember.name} memberemail={newmember.email} memberrole={newmember.role} change={handleChange}/>
+      <Form submitsetter={setMembers} memberlist={members}/>
     </div>
   );
 }
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,42 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form (props) {
 
+  const [newmember, setNewMember] = useState({ name: "", email: "", role: "" })
+
+  function handleChange(event) {
+    const updatedMember = { ...newmember, [event.target.name]: event.target.value };
+    console.log(
+      "handleChange",
+      event.target.name,
+      event.target.value,
+      updatedMember
+    );
+    setNewMember(updatedMember);
+  }
+
     return (
     <div className="new-member-form">
-      <form onSubmit={props.submit}>
-        <label>
-          Member Name:
-          <input 
-          className="form-control"
-          type="text" 
-          name="name"
-          placeholder="Enter member name"
-          value={props.membername}
-          onChange={props.change}/>
-        </label>
-        <label>
-          Email:
-          <input 
-          type="email"
-          className="form-control"
-          name="email"
-          aria-describedby="emailHelp"
-          placeholder="Enter member email"
-          value={props.memberemail}
-          onChange={props.change}/>
-        </label>
-        <label>
-          Role:
-          <input 
-          type="text"
-          className="form-control"
-          name="role"
-          placeholder="Enter member role"
-          onChange={props.change}
-          value={props.memberrole}/>
-        </label>
-        <button type="submit">Submit New Member</button>
+      <form onSubmit={() => {props.submitsetter(props.memberlist.concat([newmember]))}}>
+        <fieldset>
+          <legend>New Team Members:</legend>
+            <label>
+              Member Name:
+              <input 
+              className="form-control"
+              type="text" 
+              name="name"
+              placeholder="Enter member name"
+              value={newmember.name}
+              onChange={handleChange}/>
+            </label>
+            <label>
+              Email:
+              <input 
+              type="email"
+              className="form-control"
+              name="email"
+              aria-describedby="emailHelp"
+              placeholder="Enter member email"
+              value={newmember.email}
+              onChange={handleChange}/>
+            </label>
+            <label>
+              Role:
+              <input 
+              type="text"
+              className="form-control"
+              name="role"
+              placeholder="Enter member role"
+              onChange={handleChange}
+              value={newmember.role}/>
+            </label>
+            <button type="submit">Submit New Member</button>
+        </fieldset>
       </form>
     </div>
     );
